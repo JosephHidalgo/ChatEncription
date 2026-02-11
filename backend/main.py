784121c308c1224aa35f579154c9422f4ef6dab8
Joseph Hidalgo <hidalgoneirahenry@gmail.com>
2026-02-11 11:47:05 -0500

    Configuracion CORS

--- backend/main.py
+++ backend/main.py
@@ -55,11 +55,10 @@ app.add_middleware(
         "https://localhost:5173",
         "http://127.0.0.1:5173",
         "https://127.0.0.1:5173",
-        "http://10.236.150.102:5173",
-        "https://10.236.150.102:5173",
+        "https://chat.dalgo.dev",  
         # Permite cualquier IP de red local (10.x.x.x)
-        "http://10.*.*.*:5173",
-        "https://10.*.*.*:5173",
+        #"http://10.*.*.*:5173",
+        #"https://10.*.*.*:5173",
         
     ],
     allow_credentials=True,
